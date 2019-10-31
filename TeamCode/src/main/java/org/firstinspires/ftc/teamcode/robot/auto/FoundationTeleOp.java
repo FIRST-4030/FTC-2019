700f376dafd13c2082bdb1e078dc3941b33fa91b
Timothy Puls <timothy.puls@gmail.com>
2019-10-31 12:50:12 -0700

    start putting together teleop stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/auto/FoundationTeleOp.java
@@ -0,0 +1,147 @@
+package org.firstinspires.ftc.teamcode.robot.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.buttons.ButtonHandler;
+import org.firstinspires.ftc.teamcode.buttons.PAD_BUTTON;
+import org.firstinspires.ftc.teamcode.driveto.AutoDriver;
+import org.firstinspires.ftc.teamcode.robot.Robot;
+import org.firstinspires.ftc.teamcode.robot.common.Common;
+import org.firstinspires.ftc.teamcode.utils.OrderedEnum;
+import org.firstinspires.ftc.teamcode.utils.OrderedEnumHelper;
+import org.firstinspires.ftc.teamcode.utils.Round;
+import org.firstinspires.ftc.teamcode.vuforia.VuforiaFTC;
+
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "Grab Foundation")
+public class FoundationTeleOp extends OpMode {
+
+    // Devices and subsystems
+    private Robot robot = null;
+    private Common common = null;
+    private VuforiaFTC vuforia = null;
+    private ButtonHandler buttons;
+    private AutoDriver driver = new AutoDriver();
+
+    // Runtime vars
+    private AUTO_STATE state = AUTO_STATE.INIT;
+    private boolean gameReady = false;
+
+    @Override
+    public void init() {
+        telemetry.addData(">", "Init…");
+        telemetry.update();
+
+        // Init the common tasks elements
+        robot = new Robot(hardwareMap, telemetry);
+        common = robot.common;
+        vuforia = robot.vuforia;
+
+        // Init the camera system
+        vuforia.start();
+        vuforia.enableCapture();
+
+        // TODO: figure out what to do with this
+        initTfod();
+
+        // Register buttons
+        buttons = new ButtonHandler(robot);
+    }
+
+    @Override
+    public void init_loop() {
+        // Process driver input
+        userSettings();
+
+        // Overall ready status
+        gameReady = (robot.gyro.isReady());
+        telemetry.addData("\t\t\t", "");
+        telemetry.addData(">", gameReady ? "Ready for game start" : "NOT READY");
+
+        // Detailed feedback
+        telemetry.addData("\t\t\t", "");
+        telemetry.addData("Gyro", robot.gyro.isReady() ? "Ready" : "Calibrating…");
+
+        // Update
+        telemetry.update();
+    }
+
+    @Override
+    public void start() {
+        telemetry.clearAll();
+
+        // Log if we didn't exit init as expected
+        if (!gameReady) {
+            telemetry.log().add("Started before ready");
+        }
+
+        // Set state to starting state
+        state = AUTO_STATE.values()[0];
+
+        robot.vuforia.start();
+        robot.vuforia.enableCapture();
+
+        tfod.activate();
+    }
+
+    @Override
+    public void loop() {
+        // Handle AutoDriver driving
+        driver = common.drive.loop(driver);
+
+        // Debug feedback
+        telemetry.addData("State", state);
+        telemetry.addData("Running", driver.isRunning(time));
+        telemetry.addData("Gyro", Round.truncate(robot.gyro.getHeading()));
+        telemetry.addData("Encoder", robot.wheels.getEncoder());
+
+        // TODO: wouldn't it make more sense for AutoDriver commands to be blocking?
+        // Cut the loop short while AutoDriver is driving
+        // This prevents the state machine from running before the preceding state is complete
+        if (driver.isRunning(time)) return;
+
+        /*
+         * Main State Machine
+         * enum has descriptions of each state
+         */
+        switch (state) {
+            // TODO: lmao
+        }
+
+        // Update telemetry
+        telemetry.update();
+    }
+
+    /**
+     * Defines the order of the auto routine steps
+     */
+    enum AUTO_STATE implements OrderedEnum {
+        INIT;
+
+        public AUTO_STATE prev() { return OrderedEnumHelper.prev(this); }
+        public AUTO_STATE next() { return OrderedEnumHelper.next(this); }
+    }
+
+    /**
+     * Sets config booleans according to user input
+     */
+    private void userSettings(){
+        buttons.update();
+
+        // no settings yet
+    }
+
+    /**
+     * Utility function to delegate AutoDriver to an external provider
+     * AutoDriver is handed back up to caller when the delegate sets done to true
+     *
+     * @param autoDriver AutoDriver to be delegated
+     * @return AutoDriver once delegate finishes
+     */
+    private AutoDriver delegateDriver(AutoDriver autoDriver) {
+        if (autoDriver.isDone()) {
+            autoDriver.done = false;
+        }
+
+        return autoDriver;
+    }
+}
