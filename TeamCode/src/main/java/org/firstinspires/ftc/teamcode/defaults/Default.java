6f47fe3a0e82479d421d045541ae6058f1af45e0
Zach Isbach <zach@isbach.com>
2019-11-29 07:36:40 -0800

    Less duplication

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/defaults/Default.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/defaults/Default.java
@@ -5,11 +5,39 @@ import org.firstinspires.ftc.teamcode.RobotNG;
 import java.util.HashMap;
 
 public class Default {
-    public class DefaultData {
-        public boolean b;
-        public int i;
-        public double d;
-        public String s;
+    private class DefaultData {
+        public boolean b = false;
+        public int i = 0;
+        public double d = 0.0d;
+        public String s = "";
+
+        public DefaultData() {
+        }
+
+        public DefaultData(boolean val) {
+            set(val);
+        }
+
+        public DefaultData(int val) {
+            set(val);
+        }
+
+        public DefaultData(double val) {
+            set(val);
+        }
+
+        public DefaultData(String val) {
+            set(val);
+        }
+
+        public DefaultData dup() {
+            DefaultData d = new DefaultData();
+            d.b = this.b;
+            d.i = this.i;
+            d.d = this.d;
+            d.s = this.s;
+            return d;
+        }
 
         public void set(boolean b) {
             this.b = b;
@@ -25,14 +53,14 @@ public class Default {
         }
 
         public void set(int i) {
-            this.b = i == 0 ? false : true;
+            this.b = i != 0;
             this.i = i;
             this.d = (double) i;
             this.s = Integer.toString(i);
         }
 
         public void set(double d) {
-            this.b = d == 0 ? false : true;
+            this.b = d != 0;
             this.i = (int) d;
             this.d = d;
             this.s = Double.toString(d);
@@ -40,16 +68,12 @@ public class Default {
 
         public void set(String s) {
             if (s == null) {
-                s = new String();
+                s = "";
             }
-            if (s.isEmpty() ||
-                    s.equals("0") ||
-                    s.equalsIgnoreCase("false") ||
-                    s.equalsIgnoreCase("f")) {
-                this.b = false;
-            } else {
-                this.b = true;
-            }
+            this.b = !s.isEmpty() &&
+                    !s.equals("0") &&
+                    !s.equalsIgnoreCase("false") &&
+                    !s.equalsIgnoreCase("f");
             try {
                 this.i = Integer.parseInt(s);
             } catch (NumberFormatException e) {
@@ -64,115 +88,109 @@ public class Default {
     }
 
     private final RobotNG robot;
-    private final HashMap<String, DefaultData> m;
+    private final HashMap<String, DefaultData> orig;
+    private final HashMap<String, DefaultData> current;
 
     public Default(RobotNG robot) {
         if (robot == null) {
             throw new IllegalStateException("Robot not available");
         }
         this.robot = robot;
-        m = new HashMap<>();
+        orig = new HashMap<>();
+        current = new HashMap<>();
     }
 
     public Default() {
         this(RobotNG.R);
     }
 
-    public boolean register(String name, boolean val) {
-        if (!putValidator(name)) {
-            return false;
-        }
-        DefaultData d = new DefaultData();
-        d.set(val);
-        m.put(name, d);
-        return true;
-    }
-
-    public boolean register(String name, int val) {
-        if (!putValidator(name)) {
-            return false;
-        }
-        DefaultData d = new DefaultData();
-        d.set(val);
-        m.put(name, d);
-        return true;
-    }
-
-    public boolean register(String name, double val) {
-        if (!putValidator(name)) {
-            return false;
-        }
-        DefaultData d = new DefaultData();
-        d.set(val);
-        m.put(name, d);
-        return true;
-    }
-
-    public boolean register(String name, String val) {
-        if (!putValidator(name)) {
-            return false;
-        }
-        DefaultData d = new DefaultData();
-        d.set(val);
-        m.put(name, d);
-        return true;
-    }
-
-    private boolean putValidator(String name) {
+    private boolean register(String name, DefaultData d) {
         if (name == null || name.isEmpty()) {
             robot.log(this, "Null/empty name");
             return false;
         }
-        if (m.containsKey(name)) {
+        if (d == null) {
+            robot.log(this, "Null data");
+            return false;
+        }
+        if (current.containsKey(name)) {
             robot.log(this, "Already registered: " + name);
             return false;
         }
+        orig.put(name, d);
+        current.put(name, d.dup());
         return true;
     }
 
-    private boolean getValidator(String name) {
-        if (!m.containsKey(name)) {
+    public boolean register(String name, boolean val) {
+        DefaultData d = new DefaultData(val);
+        return register(name, d);
+    }
+
+    public boolean register(String name, int val) {
+        DefaultData d = new DefaultData(val);
+        return register(name, d);
+    }
+
+    public boolean register(String name, double val) {
+        DefaultData d = new DefaultData(val);
+        return register(name, d);
+    }
+
+    public boolean register(String name, String val) {
+        DefaultData d = new DefaultData(val);
+        return register(name, d);
+    }
+
+    public void update(String name, boolean val) {
+        DefaultData d = safeGet(name);
+        d.set(val);
+    }
+
+    public void update(String name, int val) {
+        DefaultData d = safeGet(name);
+        d.set(val);
+    }
+
+    public void update(String name, double val) {
+        DefaultData d = safeGet(name);
+        d.set(val);
+    }
+
+    public void update(String name, String val) {
+        DefaultData d = safeGet(name);
+        d.set(val);
+    }
+
+    private DefaultData safeGet(String name) {
+        DefaultData d = current.get(name);
+        if (d == null) {
+            d = new DefaultData();
             if (name == null || name.isEmpty()) {
                 name = "<NULL>";
             }
             robot.log(this, "Unregistered: " + name);
-            return false;
         }
-        return true;
+        return d;
     }
 
     public boolean getB(String name) {
-        if (!getValidator(name)) {
-            return true;
-        }
-        return m.get(name).b;
+        return safeGet(name).b;
     }
 
     public int getI(String name) {
-        if (!getValidator(name)) {
-            return 0;
-        }
-        return m.get(name).i;
+        return safeGet(name).i;
     }
 
     public float getF(String name) {
-        if (!getValidator(name)) {
-            return 0.0f;
-        }
-        return (float) m.get(name).d;
+        return (float) safeGet(name).d;
     }
 
     public double getD(String name) {
-        if (!getValidator(name)) {
-            return 0.0d;
-        }
-        return m.get(name).d;
+        return safeGet(name).d;
     }
 
     public String getS(String name) {
-        if (!getValidator(name)) {
-            return new String();
-        }
-        return m.get(name).s;
+        return safeGet(name).s;
     }
 }
