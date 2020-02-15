0f28b8f65ab10af9be77d4b9ec661aeaea63183c
Zach Isbach <zach@isbach.com>
2020-02-14 17:22:33 -0800

    New Robot

--- TeamCode/src/main/java/Robot.java
+++ TeamCode/src/main/java/Robot.java
@@ -2,10 +2,10 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 public class Robot {
     public static Robot R = null;
-    public static OpMode O = null;
+    public final OpMode opmode;
 
     public Robot(OpMode opmode) {
         R = this;
-        O = opmode;
+        this.opmode = opmode;
     }
 }
