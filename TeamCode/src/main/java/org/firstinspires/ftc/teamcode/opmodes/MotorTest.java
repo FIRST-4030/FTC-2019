4f43b85af413a86683a014f24de0e99f4831b2cb
Zach Isbach <zach@isbach.com>
2020-02-18 01:54:24 -0800

    Test Buttons and Round

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MotorTest.java
@@ -3,8 +3,10 @@ package org.firstinspires.ftc.teamcode.opmodes;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.buttons.PAD_BUTTON;
 import org.firstinspires.ftc.teamcode.core.OpModeEvents;
 import org.firstinspires.ftc.teamcode.core.OpModeN2S;
+import org.firstinspires.ftc.teamcode.utils.Round;
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "Motor Test", group = "Test")
 public class MotorTest extends OpModeN2S {
@@ -16,7 +18,7 @@ public class MotorTest extends OpModeN2S {
     private class Mode implements OpModeEvents {
 
         public void init() {
-
+            R.B.register("Reverse", gamepad1, PAD_BUTTON.a);
         }
 
         public void init_loop() {
@@ -46,13 +48,26 @@ public class MotorTest extends OpModeN2S {
             // Set motor power
             R.m1.power(p);
 
+            // Reverse on button press
+            if (R.B.get("Reverse")) {
+                R.m1.reverse(!R.m1.reverse());
+            }
+
             // Feedback
-            telemetry.addData("Battery Voltage", R.G.d("BATTERY_VOLTAGE"));
-            telemetry.addData("Battery Current", R.G.d("BATTERY_CURRENT"));
-            telemetry.addData(R.m1.name + " Power", R.m1.power());
-            telemetry.addData(R.m1.name + " Encoder", R.G.i("ENCODER_" + R.m1.name));
-            telemetry.addData(R.m1.name + " Current", R.G.d("CURRENT_" + R.m1.name));
-            telemetry.addData(R.m1.name + " Velocity", R.G.d("VELOCITY_" + R.m1.name));
+            telemetry.addData("Battery Voltage",
+                    R.G.d("BATTERY_VOLTAGE"));
+            telemetry.addData("Battery Current",
+                    R.G.d("BATTERY_CURRENT"));
+            telemetry.addData(R.m1.name + " Power",
+                    R.m1.power());
+            telemetry.addData(R.m1.name + " Reverse",
+                    R.m1.reverse());
+            telemetry.addData(R.m1.name + " Encoder",
+                    R.G.i("ENCODER_" + R.m1.name));
+            telemetry.addData(R.m1.name + " Current",
+                    Round.r(R.G.d("CURRENT_" + R.m1.name)));
+            telemetry.addData(R.m1.name + " Velocity",
+                    Round.r(R.G.d("VELOCITY_" + R.m1.name)));
         }
 
         public void stop() {
